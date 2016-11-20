60da8f8373673675fbfefd31016d137f9722f879
Cem Önal <onalcem@gmail.com>
2016-11-20 13:12:16 +0200

    Add files via upload

---
+++ src/controllers/items.controller.js
@@ -0,0 +1,14 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('ItemsController', ItemsController);
+
+ItemsController.$inject = ['items'];
+function ItemsController(items) {
+  console.log(items);
+  var $ctrl = this;
+  $ctrl.category =items.data.category;
+  $ctrl.items = items.data.menu_items;
+}
+})();
