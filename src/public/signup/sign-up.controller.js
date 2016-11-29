97fe27f1a4e5d426c4a376e4b876ce591868321a
Cem Önal <onalcem@gmail.com>
2016-11-29 15:48:05 +0200

    Add files via upload

---
+++ src/public/signup/sign-up.controller.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("public")
+    .controller("SignUpController", SignUpController);
+
+    SignUpController.$inject = ["MenuService", "UserService"];
+    function SignUpController(MenuService, UserService) {
+        var $ctrl = this;
+        $ctrl.firstName = "";
+        $ctrl.lastName = "";
+        $ctrl.email = "";
+        $ctrl.phone = "";
+        $ctrl.favoriteDishShortNumber = "";
+        $ctrl.message = "";
+        $ctrl.success = false;
+
+        $ctrl.signUp = function () {
+            MenuService.getMenuItemByShortName($ctrl.favoriteDishShortNumber).then(function (response) {
+                if (response.length > 0) {
+                    UserService.RegisterUser({
+                        firstName: $ctrl.firstName,
+                        lastName: $ctrl.lastName,
+                        email: $ctrl.email,
+                        phone: $ctrl.phone,
+                        favoriteDish: response[0]
+                    }).then(function (t) {
+                        $ctrl.success = t;
+                        $ctrl.message = "Your information has been saved!";
+                    });
+                } else {
+                    $ctrl.message = "No such menu number exists!";
+                }
+            });
+        }
+    }
+})();
---
+++ src/user/user.service.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    "use strict";
+
+    angular.module('user')
+    .service('UserService', UserService);
+
+    UserService.$inject = ["$q"];
+    function UserService($q) {
+        var service = this;
+        var _info = {};
+        var _registered = false;
+
+        service.RegisterUser = function (info) {
+            var def = $q.defer();
+            _info = info;
+            _registered = true;
+            def.resolve(_registered);
+
+            return def.promise;
+        };
+
+        service.GetUser = function () {
+            var def = $q.defer();
+            def.resolve(_info);
+            return def.promise;
+        };
+
+        service.IsRegistered = function () {
+            var def = $q.defer();
+            def.resolve(_registered);
+            return def.promise;
+        };
+    }
+})();
